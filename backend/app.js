f6993edfcda11f412ff5a333b38e92348ba84f35
Francis Bourgouin <netbeuix@gmail.com>
2020-04-04 14:55:58 -0400

    Initial backend setup

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+import logger from 'morgan';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
