474c678808bf3f83982bf38e1a7f2a324dd89dce
Francis Bourgouin <netbeuix@gmail.com>
2020-04-08 19:27:49 -0400

    Installation of client required packages

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Animal Crossing Archipelago Helper</h1>
+    </div>
+  );
+}
+
+export default App;
