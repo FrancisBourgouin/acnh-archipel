e56bc3c46626529b87abfca42d52f829d9e5370b
Francis Bourgouin <netbeuix@gmail.com>
2020-04-11 15:48:26 -0400

    Add default Schema

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,32 @@
-import express from 'express';
-import path from 'path';
-import logger from 'morgan';
+import express from "express";
+import graphqlHTTP from "express-graphql";
+import { buildSchema } from "graphql";
+import logger from "morgan";
+import path from "path";
+import indexRouter from "./routes/index";
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema: schema,
+		rootValue: global,
+		graphiql: true,
+	})
+);
 
 module.exports = app;
