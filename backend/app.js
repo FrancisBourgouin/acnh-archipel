ec8ce0dcc6f2b4a23ac4bf6cb17a304e93fc7d0f
Francis Bourgouin <netbeuix@gmail.com>
2020-04-12 12:21:42 -0400

    build basic resolver and schema

--- backend/app.js
+++ backend/app.js
@@ -15,16 +15,53 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 
 const schema = buildSchema(`
+  type Archipelago {
+    id: ID
+    name: String
+    islands : [Island]
+  }
+  type Island {
+    id: ID
+    name: String
+    nativeFruit: String
+    turnipPrices: [TurnipPrice]
+    islanders : [Islander]
+  }
+  type Islander {
+    id: ID
+    name: String
+  }
+  type TurnipPrice{
+    date: String
+    price: Int
+  }
   type Query {
     hello: String
+    archipelago: Archipelago
+    islander: Islander
+    island: Island
   }
 `);
+var root = {
+	hello: () => {
+		return "Hello world!";
+	},
+	archipelago: () => {
+		return { id: 1, name: "Covidian Montréal Consortium" };
+	},
+	island: () => {
+		return { id: 1, name: "Raftel", nativeFruit: "Peaches" };
+	},
+	islander: () => {
+		return { id: 1, name: "Francis" };
+	},
+};
 
 app.use(
 	"/graphql",
 	graphqlHTTP({
 		schema: schema,
-		rootValue: global,
+		rootValue: root,
 		graphiql: true,
 	})
 );
