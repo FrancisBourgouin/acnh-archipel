00caaccf51ff476bbf6b6badf0727c2398335e93
Francis Bourgouin <netbeuix@gmail.com>
2020-04-21 21:11:17 -0400

    feat - component creation

---
+++ client/src/components/Archipelago.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <section>
+      <h1>Archipelago Component</h1>
+    </section>
+  );
+};
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navigation from "./Navigation"
+export default () => {
+  return (
+    <header>
+      <h1>Animal Crossing Archipelago Helper</h1>
+      <Navigation />
+    </header>
+  );
+};
