70fe253c23dddfd03022a9c045b063518bcb9c07
Francis Bourgouin <netbeuix@gmail.com>
2020-04-26 09:08:32 -0400

    chore - deprecate auth with static

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,39 +1,44 @@
 import { Router } from "express";
-import { createUser, findUserById, validateUser } from "../helpers/auth";
-const auth = Router();
+import authHelpers from "../helpers/auth";
 
-auth.post("/validation", (req, res, next) => {
-	const user = findUserById(req.session.userId);
-	if (user) {
-		const { name, email, id } = user;
-		res.json({ name, email, id });
-	} else {
-		res.status(403).send("Invalid request");
-	}
-});
+export default (db) => {
 
-auth.post("/login", (req, res, next) => {
-	validateUser(req.body.email, req.body.password)
-		.then((user) => {
-			req.session.userId = user.id;
-			const { name, email, id } = user;
-			res.json({ name, email, id });
-		})
-		.catch(() => {
+	const { createUser, findUserById, validateUser } = authHelpers(db)
+	const auth = Router();
+
+	auth.post("/validation", (req, res, next) => {
+		findUserById(req.session.userId)
+			.then(user => {
+				const { name, email, id } = user;
+				res.json({ name, email, id });
+
+			})
+			.catch(() => res.status(403).send("Invalid request"));
+	});
+
+	auth.post("/login", (req, res, next) => {
+		validateUser(req.body.email, req.body.password)
+			.then((user) => {
+				req.session.userId = user._id;
+				const { name, email, id } = user;
+				res.json({ name, email, id });
+			})
+			.catch(() => {
+				res.status(403).json({});
+			});
+	});
+
+	auth.post("/register", (req, res, next) => {
+		console.log(req.body);
+		const { name, email, password, islandId } = req.body;
+		const user = createUser({ name, email, password, islandId });
+		if (user) {
+			req.session.userId = user._id;
+			res.json({ name, email, id: user.id });
+		} else {
 			res.status(403).json({});
-		});
-});
+		}
+	});
 
-auth.post("/register", (req, res, next) => {
-	console.log(req.body);
-	const { name, email, password } = req.body;
-	const user = createUser({ name, email, password });
-	if (user) {
-		req.session.userId = user.id;
-		res.json({ name, email, id: user.id });
-	} else {
-		res.status(403).json({});
-	}
-});
-
-module.exports = auth;
+	return auth
+}
