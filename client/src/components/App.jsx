0b1b721c0222eccab99be3da8134a6e07aaa4ee0
Francis Bourgouin <netbeuix@gmail.com>
2020-04-26 20:39:45 -0400

    Add URQL

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,20 +1,19 @@
+import Axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Axios from 'axios'
-
-import Archipelago from './Archipelago'
-import Header from './Header';
-import Home from './Home'
-import Login from './Login';
-import Profile from './Profile'
-import Register from './Register'
-
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { createClient, Provider } from 'urql';
 import '../styles/App.scss';
+import Archipelago from './Archipelago';
+import Header from './Header';
+import Home from './Home';
+import Login from './Login';
+import Profile from './Profile';
+import Register from './Register/Main';
+
+const client = createClient({
+  url: '/graphql',
+});
+
 const App = () => {
   const [user, setUser] = useState({})
 
@@ -26,28 +25,30 @@ const App = () => {
   }, [])
 
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/register">
-            <Register {...{ user, setUser }} />
-          </Route>
-          <Route exact path="/login">
-            <Login {...{ user, setUser }} />
-          </Route>
-          <Route exact path="/profile">
-            <Profile />
-          </Route>
-          <Route exact path="/archipelago">
-            <Archipelago />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Provider value={client}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/register">
+              <Register {...{ user, setUser }} />
+            </Route>
+            <Route exact path="/login">
+              <Login {...{ user, setUser }} />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Route exact path="/archipelago">
+              <Archipelago />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
