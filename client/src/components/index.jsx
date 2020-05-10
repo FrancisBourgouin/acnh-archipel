5d43961bdb16392f83eb6ade70a04a22337356df
Corrina Chow <corrina.yy.chow@gmail.com>
2020-05-10 19:52:54 -0400

    feat - Add Market Update

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -5,7 +5,19 @@ import Home from "./Home";
 import Island from "./Island";
 import Login from "./Login";
 import Market from "./Market";
+import MarketUpdate from "./MarketUpdate";
 import Profile from "./Profile";
 import Register from "./Register";
 
-export { Archipelago, Dashboard, Header, Home, Island, Login, Market, Profile, Register };
+export {
+	Archipelago,
+	Dashboard,
+	Header,
+	Home,
+	Island,
+	Login,
+	Market,
+	Profile,
+	Register,
+	MarketUpdate,
+};
