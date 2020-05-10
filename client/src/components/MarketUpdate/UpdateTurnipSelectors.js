7ccb40998747c3874853ca771044530180521a46
Corrina Chow <corrina.yy.chow@gmail.com>
2020-05-10 19:53:06 -0400

    feat - Add Update Turnip Selectors

---
+++ client/src/components/MarketUpdate/UpdateTurnipSelectors.js
@@ -0,0 +1,24 @@
+export function createTurnipPayload(enteredTurnipData, islandId) {
+	const { date, timeOfDay, price } = enteredTurnipData;
+	const year = date.getFullYear();
+	const month = date.getMonth();
+	const day = date.getDate();
+	const time = timeOfDay === "am" ? 1 : 13;
+	const parsedDate = new Date(year, month, day, time);
+	return {
+		date: parsedDate,
+		price: parseInt(price),
+		islandId: islandId,
+	};
+}
+
+export function findIslandByIslandId(islands = [], islandId) {
+	let islandIndex = 0;
+	for (let island of islands) {
+		if (island._id === islandId) {
+			return islandIndex;
+		}
+		islandIndex++;
+	}
+	return undefined;
+}
